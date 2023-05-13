d6e195075a1d0cb942fd9751d2c4af57314b5353
parallelqw <privatelle@tutanota.com>
2023-05-13 18:14:13 +0300

    a20 direct device

--- a10/device.mk
+++
@@ -1,9 +0,0 @@
-TARGET_LOCAL_ARCH := arm64
-
-$(call inherit-product, vendor/samsung/a10/a10-vendor.mk)
-
-# Sensors
-PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-service.a10
-
-include device/samsung/a10/common.mk
